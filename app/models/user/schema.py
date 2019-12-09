de5112724dde47625eb64a410d7af586aaa5044c
pardhu448 <pardhuuvce@gmail.com>
2019-12-09 18:49:02 +0530

    minor changes long back

--- app/models/user/schema.py
+++ app/models/user/schema.py
@@ -5,6 +5,7 @@ import jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), unique=False)
