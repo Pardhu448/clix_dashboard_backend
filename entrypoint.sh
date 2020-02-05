a833141a8bba6362b3f39a545f9c858d8f743286
mrunal4 <mrunal4888@gmail.com>
2020-02-05 19:50:26 +0530

    add ssl cert provision for gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,16 @@ done
 
 echo "PostgreSQL started"
 
-flask db init;
-flask db migrate;
+if [[ -d /home/clix_dashboard_backend/migrations ]]; then
+  echo "Migration folder already exists."
+else
+  echo "Migration folder doesn't exists. "
+  echo "Hence triggering flask db init."
+  flask db init;
+  echo "And triggering flask db migrate."
+  flask db migrate;
+fi
+
 flask create_admin --username admin_clixdata --password clixdata --email admin@clix_dashboard_backend.com ;
 
 while true; do
@@ -23,8 +31,18 @@ while true; do
     sleep 5
 done
 
+if [[ $FLASK_ENV == "prduction" ]]
+  c_file="/home/clix_dashboard_backend/certs/cert.pem"
+  k_file="/home/clix_dashboard_backend/certs/key.pem"
+  if [[ -f ${c_file} ]] && [[ -f ${k_file} ]]; then
+    ssl_parameters = "--certfile=${c_file} --keyfile=${k_file}"
+  else
+    echo "Files missing - ${c_file} and ${k_file}"
+  fi
+fi
+
 # starting gunicorn service on port 5000
 echo "Starting gunicorn service on port 5000"
-exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - clix_dashboard_backend:app
+exec gunicorn ${ssl_parameters} -b 0.0.0.0:5000 --access-logfile - --error-logfile - clix_dashboard_backend:app
 
 # exec "$@"
